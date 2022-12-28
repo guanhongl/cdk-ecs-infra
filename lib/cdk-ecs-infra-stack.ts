4f7b71b10597cd19e3bdc7c3c95df17823c9a57c
guanhongl <guanh.sl23@gmail.com>
2022-12-27 21:48:58 -1000

    create the resource stack
    - IAM role
    - ECS Task def
    - ECS Pattern for Fargate lb

--- lib/cdk-ecs-infra-stack.ts
+++ lib/cdk-ecs-infra-stack.ts
@@ -1,6 +1,9 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ecs from 'aws-cdk-lib/aws-ecs'; // Allows working with ECS resources
+import * as ec2 from 'aws-cdk-lib/aws-ec2'; // Allows working with EC2 and VPC resources
+import * as iam from 'aws-cdk-lib/aws-iam'; // Allows working with IAM resources
+import * as ecsPatterns from 'aws-cdk-lib/aws-ecs-patterns'; // Helper to create ECS services with loadbalancers, and configure them
 
 export class CdkEcsInfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -8,9 +11,45 @@ export class CdkEcsInfraStack extends cdk.Stack {
 
     // The code that defines your stack goes here
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkEcsInfraQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Look up the default VPC
+    const vpc = ec2.Vpc.fromLookup(this, "VPC", {
+      isDefault: true
+    });
+
+    // define which container to use, and how it should be configured
+
+    // create an empty IAM role to attach to the task definition
+    const taskIamRole = new iam.Role(this, "AppRole", {
+      roleName: "AppRole",
+      assumedBy: new iam.ServicePrincipal('ecs-tasks.amazonaws.com'),
+    });
+
+    // specify a task definition type to deploy to Fargate
+    const taskDefinition = new ecs.FargateTaskDefinition(this, 'Task', {
+      taskRole: taskIamRole,
+    });
+
+    // create a task definition to supply the container port, 
+    // amount of cpu and memory it needs, and the container image to use
+    taskDefinition.addContainer('MyContainer', {
+    // build the container image provided with the sample application in SampleApp folder
+    // CDK will build the container image using the Dockerfile in the SampleApp directory
+      image: ecs.ContainerImage.fromRegistry('hello-world'),
+      portMappings: [{ containerPort: 80 }],
+      memoryReservationMiB: 256,
+      cpu : 256,
+    });
+
+    // set up the ECS cluster, define a service, create a load balancer, 
+    // configure it to connect to the service, and set up the required security group rules
+    // the ECS Pattern will configure the security group
+    new ecsPatterns.ApplicationLoadBalancedFargateService(this, "MyApp", {
+      vpc: vpc, // specify where to create all the resources
+      taskDefinition: taskDefinition, // define which container image to deploy
+      desiredCount: 1, // launch 1 copy of the container
+      serviceName: 'MyWebApp',
+      assignPublicIp: true, // allow public subnets
+      publicLoadBalancer: true, // allow public access
+    });
   }
 }
